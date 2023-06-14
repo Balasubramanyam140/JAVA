e73e3d3426460dbe1e318c98607767a2b4287d04
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2023-06-14 20:09:41 +0530

    __methods.java

---
+++ __methods.java
@@ -0,0 +1,19 @@
+//Creating a Method and Calling the Method
+public class Main{
+    static void myMethod(){
+        System.out.println("Hey Hi!!");
+    }
+    public static void main(String[] args){
+        myMethod();
+      /*
+      myMethod() prints a text (the action), when it is called. To call a method,
+      write the method's name followed by two parentheses () and a semicolon;
+      */
+    }
+}
+
+/*
+  OUTPUT :
+             Hey Hi!!
+*/
+_____________________________________________________________________________________________
