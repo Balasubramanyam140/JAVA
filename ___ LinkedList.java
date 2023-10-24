faa520d9676cdbeb6f3b9e08b01e58b1f7e5a186
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2023-10-24 20:43:13 +0530

    ___ LinkedList.java

--- ___ LinkedList.java
+++ ___ LinkedList.java
@@ -19,3 +19,30 @@ OUTPUT :
 
 */
 -------------------------------------------------------------------------------------------------
+QUE : 1. Creating linked list using LinkedList<String> variableName = new LinkedList<String>();
+      2. Adding elements using add() method .
+      3. Adding element at the beggining of the list using addFirst() method .
+      
+import java.util.LinkedList;
+
+public class Main{
+    public static void main(String[] args){
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        System.out.println("Actual LinkedList \n" + cars);
+        cars.addFirst("Mazda");
+        System.out.println("Adding new item at 1st in LinkedList \n" + cars);
+    }
+}
+
+/*
+OUTPUT :
+            Actual LinkedList 
+            [Volvo, BMW, Ford]
+            Adding new item at 1st in LinkedList 
+            [Mazda, Volvo, BMW, Ford]
+*/
+
+---------------------------------------------------------------------------------------------
