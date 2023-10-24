ef9f6e7611c4298ef75aeeaadc6ab1258b8c665b
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2023-10-24 20:51:20 +0530

    ___ LinkedList.java

--- ___ LinkedList.java
+++ ___ LinkedList.java
@@ -46,3 +46,35 @@ OUTPUT :
 */
 
 ---------------------------------------------------------------------------------------------
+QUE :  Adding an item to the end of the list
+      1. Create a linkedList using LinkedList<String> variableName = new LinkedList<String>();
+      2. Add elements in LinkedList using add() method .
+      3. Add an element at the last using addLast() method .
+
+
+import java.util.LinkedList;
+
+public class Main{
+    public static void main(String[] args){
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        System.out.println("Actual LinkedList \n" + cars);
+        cars.addLast("Mazda");
+        System.out.println("Adding new item at Last in LinkedList \n" + cars);
+    }
+}
+
+/*
+OUTPUT :
+            Actual LinkedList 
+            [Volvo, BMW, Ford]
+            Adding new item at Last in LinkedList 
+            [Volvo, BMW, Ford, Mazda]
+
+*/
+
+-----------------------------------------------------------------------------------------------
+
+      
