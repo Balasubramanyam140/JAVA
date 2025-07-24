0e2c4a24364573d20cf8e322041687e39fdcff54
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2025-07-24 12:48:55 +0530

    09. Perfect Square.java

--- 09. Perfect Square.java
+++ 09. Perfect Square.java
@@ -1,5 +1,5 @@
 QUE 1 : Check Weather given Number is PErfect Sq or not
-
+Que 2: No of Perfect Squares in an Array
 
 
 _________________________________________________________________________
@@ -18,3 +18,39 @@ public class Main {
     }
 }
 _________________________________________________________________________
+Que 2: No of Perfect Squares in an Array
+
+    import java.util.Scanner;
+            public class Main {
+                public static void main(String[] args) {
+                    Scanner sc = new Scanner(System.in);
+                    int n = sc.nextInt();
+                    int[] arr = new int[n];
+                    for(int i=0;i<n;i++){
+                        arr[i] = sc.nextInt();
+                    }
+                    int count =0;
+                    for(int i=0;i<n;i++){
+                        if(isPerfectSquare(arr[i])){
+                            count++;
+                        }
+                    }
+                    System.out.println("Number of Perfect SQuare : "+count);
+                }
+                
+                static boolean isPerfectSquare(int a){
+                    if(a < 0 ) return false;
+                    int sqrt = (int) Math.sqrt(a);
+                    return sqrt*sqrt ==a;
+                }
+        }
+
+OUTPUT:
+            5
+            1
+            23
+            45
+            36
+            81
+            Number of Perfect SQuare : 3
+--------------------------------------------------------------------------
