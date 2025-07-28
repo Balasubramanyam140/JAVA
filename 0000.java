a9a66e84757d70ee9df5b6aa469860668f76a8d9
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2025-07-28 16:07:28 +0530

    Update 0000.java

--- 0000.java
+++ 0000.java
@@ -152,7 +152,26 @@ QUE 5:  Sum of Odd Numbers
 ------------------------------------------------------------------------
 Que 7: Transpose of Array elements
 
-        
+        import java.util.Scanner;
+        class Main{
+            public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                int row = sc.nextInt();
+                int col = sc.nextInt();
+                int arr[][] = new int[row][col];
+                for(int i=0;i<row;i++){
+                    for(int j=0;j<col;j++){
+                        arr[i][j]= sc.nextInt();
+                    }
+                }
+                for(int i=0;i<col;i++){
+                    for(int j=0;j<row;j++){
+                        System.out.print(arr[j][i]+" ");
+                    }
+                    System.out.println();
+                }
+            }
+        }
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
