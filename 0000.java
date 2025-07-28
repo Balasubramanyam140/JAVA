8e423f5825ff1c549a01980519baa8f40f50c95b
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2025-07-28 15:28:31 +0530

    Update 0000.java

--- 0000.java
+++ 0000.java
@@ -1,6 +1,6 @@
 Que 1: Decimal to Binary
 Que 1: Binary to Decimal
-  
+Que 2: Count of digits in an array
 Que 4: No of Perfect Squares in an Array
 
 
@@ -35,6 +35,35 @@ Que 1: Binary to Decimal
             }
         }
 ------------------------------------------------------------------------
+Que 2: Count of digits in an array
+          import java.util.Scanner;
+        class Main{
+            public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+                int[] arr = new int[n];
+                for(int i=0;i<n;i++){
+                    arr[i]= sc.nextInt();
+                }
+                int sum =0;
+                for(int i=0;i<n;i++){
+                    sum += countArr(arr[i]);
+                }
+                System.out.println(sum);
+            }
+            static int countArr(int a){//10
+                if(a==0){
+                    return 0;
+                }
+                int sum=0;
+                while(a>0){//10   1
+                    int rem = a%10;//0  1
+                    sum= sum+rem;//0   1
+                    a=a/10;//1  0
+                }
+                return sum;
+            }
+        }
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
